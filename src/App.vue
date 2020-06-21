d81403351b4e25038bbf58277309c99810285f8e
circlesharp <circlesharp@live.com>
2020-06-21 11:18:35 +0800

    改变结构
    Signed-off-by: circlesharp <circlesharp@live.com>

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,152 @@
 <template>
-<div>
-  <h1>Learning iview</h1>
-  <h2>basic-component</h2>
-  <ButtonGroup shape="circle" style="margin: 20px 10px;">
-    <Button to="/button" type="primary" ghost>Button</Button>
-    <Button to="/icon" type="primary" ghost>Icon</Button>
-    <Button to="/grid" type="primary" ghost>Grid</Button>
-  </ButtonGroup>
-  <h2>layout</h2>
-  <ButtonGroup shape="circle" style="margin: 20px 10px;">
-    <Button to="/layout/basic" type="primary" ghost>LayoutBasic</Button>
-    <Button to="/layout/layout-1" type="primary" ghost>Layout-1</Button>
-    <Button to="/layout/layout-2" type="primary" ghost>Layout-2</Button>
-    <Button to="/layout/layout-3" type="primary" ghost>Layout-3</Button>
-  </ButtonGroup>
-  <router-view></router-view>
-</div>
+  <div class="layout">
+    <Layout>
+      <Sider
+        ref="side1"
+        hide-trigger
+        collapsible
+        :width="160"
+        :collapsed-width="78"
+        @on-collapse="siderCollapsed"
+        v-model="isCollapsed"
+      >
+        <Menu
+          active-name="1-2"
+          theme="dark"
+          width="auto"
+          :class="menuitemClasses"
+        >
+          <Submenu name="1">
+            <template slot="title">
+              <Icon type="ios-navigate"></Icon>
+              <span>basic-component</span>
+            </template>
+            <MenuItem name="1-1" to="/basic/button">Button</MenuItem>
+            <MenuItem name="1-2" to="/basic/icon">Icon</MenuItem>
+            <MenuItem name="1-3" to="/basic/grid">Grid</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-keypad"></Icon>
+              <span>layout</span>
+            </template>
+            <MenuItem name="2-1" to="/layout/basic">LayoutBasic</MenuItem>
+            <MenuItem name="2-2" to="/layout/layout-1">Layout-1</MenuItem>
+            <MenuItem name="2-3" to="/layout/layout-2">Layout-2</MenuItem>
+            <MenuItem name="2-4" to="/layout/layout-3">Layout-3</MenuItem>
+          </Submenu>
+          <Submenu name="3">
+            <template slot="title">
+              <Icon type="ios-analytics"></Icon>
+              <span>form</span>
+            </template>
+          </Submenu>
+        </Menu>
+      </Sider>
+
+      <Layout>
+        <Header
+          class="layout-header-bar"
+          style="padding: 0;"
+        >
+          <Icon
+            type="md-menu"
+            size="24"
+            @click.native="collapsedSider"
+            :class="rotateIcon"
+            :style="{margin: '0 20px'}"
+          ></Icon>
+        </Header>
+        <Content :style="{margin: '20px', minHeight: '280px', background: '#fff', overflow: 'hidden'}">
+          <router-view></router-view>
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
+  data() {
+    return {
+      isCollapsed: false,
+    }
+  },
+  computed: {
+    rotateIcon() {
+      return [
+        'menu-icon',
+        this.isCollapsed ? 'rotate-icon' : ''
+      ]
+    },
+    menuitemClasses() {
+      return [
+        'menu-item',
+        this.isCollapsed ? 'collapsed-menu' : ''
+      ]
+    },
+  },
+  methods: {
+    collapsedSider() {
+      this.$refs.side1.toggleCollapse()
+      // this.isCollapsed = !this.isCollapsed
+    },
+    siderCollapsed() {
+      // console.log('siderCollapsed')
+    },
+  }
 }
 </script>
 
-<style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-header-bar{
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
+    .menu-icon{
+        transition: all .3s;
+    }
+    .rotate-icon{
+        transform: rotate(-90deg);
+    }
+    .menu-item span{
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: width .2s ease .2s;
+    }
+    .menu-item i{
+        transform: translateX(0px);
+        transition: font-size .2s ease, transform .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+    .collapsed-menu span{
+        width: 0px;
+        transition: width .2s ease;
+    }
+    .collapsed-menu i{
+        transform: translateX(5px);
+        transition: font-size .2s ease .2s, transform .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
+    }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import VueRouter from 'vue-router'
-import Button from './components/button'
-import Icon from './components/icon'
-import Grid from './components/grid'
+import Button from './components/basic/button'
+import Icon from './components/basic/icon'
+import Grid from './components/basic/grid'
 import LayoutBasic from './components/layout/basic'
 import Layout_1 from './components/layout/layout-1'
 import Layout_2 from './components/layout/layout-2'
@@ -9,13 +9,14 @@ import Layout_3 from './components/layout/layout-3'
 
 const routes = [
   { path: '/' },
-  { path: '/button', component: Button },
-  { path: '/icon', component: Icon },
-  { path: '/grid', component: Grid },
+  { path: '/basic/button', component: Button },
+  { path: '/basic/icon', component: Icon },
+  { path: '/basic/grid', component: Grid },
   { path: '/layout/basic', component: LayoutBasic },
   { path: '/layout/layout-1', component: Layout_1 },
   { path: '/layout/layout-2', component: Layout_2 },
   { path: '/layout/layout-3', component: Layout_3 },
+  { path: '/form/input', component: Layout_3 },
 ]
 
 const router = new VueRouter({
