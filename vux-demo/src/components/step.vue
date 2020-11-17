8019400c0d7fefe69b33752d8d268f02996ab075
circlesharp <circlesharp@live.com>
2020-11-17 16:30:49 +0800

    feat: 实现了通用的 step, 样式完美

---
+++ vux-demo/src/components/step.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="wrap-steps">
+    <div class="steps">
+      <div class="step" :class="idx === steps.length - 1 ? 'last-step' : 'not-last-step'"  v-for="(item, idx) in steps" :key="idx">
+        <div class="head" :class="onClassHead(idx)"></div>
+        <div class="rear" :class="onClassRear(idx)" v-if="idx < steps.length - 1"></div>
+        <div class="wrap-desc">
+          <div class="desc" :class="onClassDesc(idx)">{{ item }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    steps: {
+      type: Object,
+      default() {
+        return ['验证账号', '发送验证码', '修改密码', '完成'];
+      },
+    },
+    current: {
+      type: Number,
+      default: 2,
+    },
+  },
+  methods: {
+    onClassHead(idx) {
+      if (idx < this.current) return 'actived';
+      if (idx === this.current) return 'activing';
+      if (idx > this.current) return 'unactived';
+    },
+    onClassRear(idx) {
+      if (idx < this.current) return 'actived';
+      else return 'unactived';
+    },
+    onClassDesc(idx) {
+      if (idx < this.current) return 'actived';
+      if (idx === this.current) return 'activing';
+      if (idx > this.current) return 'unactived';
+    },
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@head-actived: purple;
+@head-activing: red;
+@head-unactived: pink;
+@rear-actived: blue;
+@rear-unactived: skyblue;
+@desc-active: black;
+@desc-unactived: grey;
+
+.wrap-steps {
+  padding-bottom: 20px;
+}
+.steps {
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  .step {
+    position: relative;
+    display: flex;
+    align-items: center;
+    .head {
+      width: 10px;
+      height: 10px;
+      &.actived {
+        background: @head-actived;
+      }
+      &.activing {
+        background: @head-activing;
+      }
+      &.unactived {
+        background: @head-unactived;
+      }
+    }
+    .rear {
+      flex: 1;
+      height: 1px;
+      &.actived {
+        background: @rear-actived;
+      }
+      &.unactived {
+        background: @rear-unactived;
+      }
+    }
+    .wrap-desc {
+      position: absolute;
+      bottom: -10px;
+      display: flex;
+      justify-content: center;
+      height: 20px;
+      width: 10px;
+      .desc {
+        display: inline-block;
+        flex-shrink: 0;
+        height: 10px;
+        font-size: 8px;
+        line-height: 13px;
+        &.actived {
+          color: @desc-active;
+        }
+        &.activing {
+          color: @desc-active;
+          font-size: 13px;
+          font-weight: 500;
+        }
+        &.unactived {
+          color: @desc-unactived;
+        }
+      }
+    }
+  }
+  .step.not-last-step {
+    flex: 1;
+  }
+}
+</style>
---
+++ vux-demo/src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <Step :current="3"/>
+  </div>
+</template>
+
+<script>
+import Step from './step';
+
+export default {
+  components: {Step},
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
