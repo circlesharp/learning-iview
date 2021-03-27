03613613ec09878d724de658618b34931cfc0f86
circlesharp <circlesharp@live.com>
2021-03-27 21:05:07 +0800

    后端接收前端参数的4种方式

--- hello-koa/api/v1/book.js
+++ hello-koa/api/v1/book.js
@@ -2,7 +2,17 @@ const Router = require("koa-router");
 
 const router = new Router();
 
-router.get('/v1/book/latest', (ctx, next) => {
+/**
+ * ps 前端向后端的 4 种常见传参方式
+ * 1. params (动态路由)
+ * 2. query
+ * 3. header
+ * 4. body
+ */
+
+router.post('/v1/:id/book/latest', (ctx, next) => {
+	const { params } = ctx;
+	const { query, header, body } = ctx.request;
 	ctx.body = {
 		key: 'book'
 	};
